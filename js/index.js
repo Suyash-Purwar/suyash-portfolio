d12ee0eba15134d0c257510d9038e7b7456b3500
suyash-purwar <purwarsuyash06@gmail.com>
2019-11-03 17:30:21 +0530

    Restructured files and folders for deployment

---
+++ js/index.js
@@ -0,0 +1,54 @@
+document.querySelector("#sidemenu_trigger").addEventListener('click', () => {
+    document.querySelector(".sidebar-content-box").style.transform = "translateX(0%)";
+
+    document.querySelector(".sidebar-back-box").style.transform = "translateY(0%)";
+
+    document.querySelector("#sidebar").style.display="block"
+});
+
+document.querySelector("#sidebar-close").addEventListener('click', () => {
+    document.querySelector(".sidebar-content-box").style.transform = "translateX(-100%)";
+
+    document.querySelector(".sidebar-back-box").style.transform = "translateY(-100%)";
+    
+    setTimeout(() => {
+        document.querySelector("#sidebar").style.display="none"
+    }, 500);
+})
+
+let projects_carousel = new Siema({
+    selector: ".siema",
+    onChange: function() {
+        const proj_indicators = document.getElementsByClassName("projects-indicator");
+
+        proj_indicators[this.currentSlide].style.backgroundColor = "#000";
+
+        for (let x = 0; x < proj_indicators.length; x++) {
+            if (x == this.currentSlide) continue;
+
+            proj_indicators[x].style.backgroundColor = "#fff";
+        }
+    }
+
+})
+
+let prev_proj = document.querySelector("#prev_proj");
+let next_proj = document.querySelector("#next_proj");
+
+prev_proj.addEventListener('click', () => projects_carousel.prev(1))
+next_proj.addEventListener('click', () => projects_carousel.next(1))
+
+let blogs_carousel = new Siema({
+    selector: '.blogs-carousel',
+    onChange: function() {
+        const blogs_indicators = document.getElementsByClassName("blogs-indicator");
+
+        blogs_indicators[this.currentSlide].style.backgroundColor = "#000";
+
+        for (let x = 0; x < blogs_indicators.length; x++) {
+            if (x == this.currentSlide) continue;
+
+            blogs_indicators[x].style.backgroundColor = "#fff";
+        }
+    }
+})
